2dcbce94c8f4c1d2488fd87cd4abcf1c6c707ae8
Curtis Rau <curtisrau@gmail.com>
2016-06-27 17:37:56 -0400

    deleted all that is useless

--- gustafsonAlgorithm/DerivedData/gustafsonAlgorithm/Build/Intermediates/gustafsonAlgorithm.build/Debug/gustafsonAlgorithm.build/Objects-normal/x86_64/main.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Volumes/userFilesPartition/Users/curtisrau/Documents/Work/LIGO/gustafsonAlgorithm/gustafsonAlgorithm/gustafsonAlgorithm/main.cpp \
-  /Volumes/userFilesPartition/Users/curtisrau/Documents/Work/LIGO/gustafsonAlgorithm/gustafsonAlgorithm/gustafsonAlgorithm/gustafsonAlgorithm.hpp \
-  /Volumes/userFilesPartition/Users/curtisrau/Documents/Work/LIGO/gustafsonAlgorithm/gustafsonAlgorithm/gustafsonAlgorithm/besselFunctions.hpp
--- makeFakeData/DerivedData/makeFakeData/Build/Intermediates/makeFakeData.build/Debug/makeFakeData.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Volumes/userFilesPartition/Users/curtisrau/Documents/Work/LIGO/gustafsonAlgorithm/makeFakeData/makeFakeData/main.cpp
